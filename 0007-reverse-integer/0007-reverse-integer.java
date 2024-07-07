5408d3a74b619a8dcced5ad2eb29c95bf0d4dbe5
Sougat1 <123895902+Sougat1@users.noreply.github.com>
2024-07-08 00:14:35 +0530

    Time: 1 ms (75.45%), Space: 40.8 MB (30.82%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int sign=1;
+        if(x<0){
+            sign=-1;
+            x=-x;
+        }
+        long ans=0;
+        while(x>0){
+            ans=(ans*10)+(x%10);
+            if(ans>Integer.MAX_VALUE){
+                ans=0;
+            }
+            x/=10;
+        }
+        return (int)(ans*sign);
+    }
+}
