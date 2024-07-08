410d0c8b7a4206a443f5320457e32522ae7ec859
Sougat1 <123895902+Sougat1@users.noreply.github.com>
2024-07-08 07:16:33 +0530

    Time: 0 ms (100%), Space: 41.8 MB (27.46%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,25 +3,60 @@ class Solution {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int dp[][]=new int[m][n];
+
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 && j==0 && obstacleGrid[i][j]==0){
+                    dp[0][0]=1;
+                }else{
+                    int up=0;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    int left=0;
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=up+left;
+
+                }
+            }
         }
-        return helper(m-1,n-1,obstacleGrid,dp);
+        return dp[m-1][n-1];
+
+
+
+
+
+
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return helper(m-1,n-1,obstacleGrid,dp);
     }
 
-    int helper(int i,int j,int[][] obstacleGrid,int [][]dp){
-        if(i<0 || j<0 || obstacleGrid[i][j]==1){
-            return 0;
-        }
-        if(i==0 && j==0){
-            return 1;
-        }
+
+
+
+
+
+    // int helper(int i,int j,int[][] obstacleGrid,int [][]dp){
+    //     if(i<0 || j<0 || obstacleGrid[i][j]==1){
+    //         return 0;
+    //     }
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
         
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=helper(i-1,j,obstacleGrid,dp);
-        int left=helper(i,j-1,obstacleGrid,dp);
-        return dp[i][j]=up+left;
-    }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int up=helper(i-1,j,obstacleGrid,dp);
+    //     int left=helper(i,j-1,obstacleGrid,dp);
+    //     return dp[i][j]=up+left;
+    // }
 }
