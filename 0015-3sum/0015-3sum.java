5171017359b5543c1d9c9a56225f400014770ad8
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2024-07-30 20:41:21 +0530

    Time: 1313 ms (7.29%), Space: 55.1 MB (5.17%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+        int n=nums.length;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        HashSet<List<Integer>>set=new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],i);
+        }
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int rem=-(nums[i]+nums[j]);
+                if(map.containsKey(rem)){
+                    int ind=map.get(rem);
+                    if(ind!=i && ind!=j){
+                        List<Integer>list=new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(rem);
+                        Collections.sort(list);
+                        set.add(list);
                     }
+                    
                 }
             }
         }
-        return res;  
+        List<List<Integer>>ans=new ArrayList<>(set);
+        return ans;
     }
 }
