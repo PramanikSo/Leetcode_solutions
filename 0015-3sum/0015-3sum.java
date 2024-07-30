67c4566ef113c8f2bd67dd5e1b84d6c792af357c
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2024-07-30 20:59:19 +0530

    Time: 2354 ms (5.01%), Space: 53.5 MB (8.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
-        HashMap<Integer,Integer>map=new HashMap<>();
+        
         HashSet<List<Integer>>set=new HashSet<>();
 
         for(int i=0;i<n;i++){
-            map.put(nums[i],i);
-        }
+             HashSet<Integer>hset=new HashSet<>();
 
-
-        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int rem=-(nums[i]+nums[j]);
-                if(map.containsKey(rem)){
-                    int ind=map.get(rem);
-                    if(ind!=i && ind!=j){
-                        List<Integer>list=new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(rem);
+                if(hset.contains(rem)){
+                        List<Integer>list=Arrays.asList(nums[i],nums[j],rem);
                         Collections.sort(list);
                         set.add(list);
-                    }
-                    
                 }
+                hset.add(nums[j]);
             }
         }
         List<List<Integer>>ans=new ArrayList<>(set);
