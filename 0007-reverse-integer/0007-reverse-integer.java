260901fc60f4c49c26a5af9013b8b791a42e4dc1
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2024-12-03 09:08:23 +0530

    Time: 1 ms (86.2%), Space: 40.7 MB (76.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -8,11 +8,12 @@ class Solution {
         long ans=0;
         while(x>0){
             ans=ans*10+(x%10);
-            if(ans>Integer.MAX_VALUE){
-                return 0;
-            }
             x=x/10;
         }
-        return (int)ans*sign;
+        ans=ans*sign;
+         if(ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE){
+                return 0;
+         }
+        return (int)ans;
     }
 }
