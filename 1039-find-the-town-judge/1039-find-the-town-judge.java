03610b4f83791d6bb70f892a41a47f821ba534c1
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-08-26 22:45:57 +0530

    Time: 3 ms (61.03%), Space: 54.6 MB (7.94%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,31 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int adj[][] = new int[n+1][n+1];
-        for(int i=0;i<trust.length;i++){
+          int count[] = new int[n+1];
+          for(int i=0;i<trust.length;i++){
             int num1=trust[i][0];
             int num2=trust[i][1];
-            adj[num1][num2]=1;
-        }
-        int judge=-1;
-        for(int i=1;i<=n;i++){
-            int sum=0;
-            for(int j=1;j<=n;j++){
-                sum+=adj[i][j];
+            count[num1]--;
+            count[num2]++;
+          }
+          for(int i=1;i<=n;i++){
+            if(count[i]==n-1){
+                return i;
             }
-            if(sum==0){
-                judge=i;
-                break;
-            }
-        }
-        int sum=0;
-        if(judge!=-1){
-        for(int i=1;i<=n;i++){
-            sum+=adj[i][judge];
-        }
-        if(sum==n-1){
-            return judge;
-        }
-        }
-        return -1;
+          }
+          return -1;
     }
 }
