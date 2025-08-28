ea3f53ddbc7ee878bab3c9077f4cf55d2e2d7182
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-08-28 22:05:32 +0530

    Time: 414 ms (22.6%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,6 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 -- SELECT EMAIL FROM Person GROUP BY EMAIL HAVING COUNT(EMAIL)>1;
+
 SELECT DISTINCT P1.EMAIL FROM PERSON P1 JOIN PERSON P2 
 ON P1.EMAIL=P2.EMAIL AND
  P1.ID <> P2.ID; 
