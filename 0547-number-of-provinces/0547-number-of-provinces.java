35e8ee90e064bab9ff9c199c27f7fad500c78085
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-08-29 08:10:30 +0530

    Time: 1 ms (87.26%), Space: 48.2 MB (20.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,26 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        List<List<Integer>>adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<Integer>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj.get(j).add(i);
-                    adj.get(i).add(j);
-                }
-                
-            }
-        }
         int count=0;
         boolean visited[]=new boolean[n];
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 count++;
-                bfs(i,adj,visited);
+                bfs(i,isConnected,visited);
             }
         }
         return count;
 
     }
 
-    private void bfs(int node,List<List<Integer>>adj,boolean visited[]){
-       Queue<Integer>q=new LinkedList<>();
+    private void bfs(int node,int[][] isConnected,boolean visited[]){
+       Deque<Integer>q=new ArrayDeque<>();
        q.add(node);
        visited[node]=true;
        while(!q.isEmpty()){
-        int pop=q.poll();
-        for(int i : adj.get(pop)){
-            if(!visited[i]){
+        int curr=q.poll();
+        for(int i=0;i<isConnected.length;i++){
+            if(isConnected[curr][i]==1 && !visited[i]){
                 visited[i]=true;
                 q.add(i);
             }
