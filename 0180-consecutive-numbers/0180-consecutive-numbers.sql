e6920f7751b8e4e2a372f284effa028133f8b750
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-09-02 23:58:08 +0530

    Time: 580 ms (73.86%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,4 +1,14 @@
 /* Write your PL/SQL query statement below */
+-- SELECT DISTINCT(L1.NUM) AS ConsecutiveNums  
+-- FROM LOGS L1, LOGS L2, LOGS L3
+-- WHERE
+-- L1.ID = L2.ID - 1
+-- AND L2.ID = L3.ID - 1
+-- AND L1.NUM = L2.NUM
+-- AND L2.NUM = L3.NUM;
+
+-- Alternative
+
 SELECT DISTINCT Num AS ConsecutiveNums
 FROM (
     SELECT Num,
