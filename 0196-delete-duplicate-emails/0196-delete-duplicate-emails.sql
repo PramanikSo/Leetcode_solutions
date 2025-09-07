a873aabfe02ea8523f341aaf77f314787868e0b3
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-09-07 09:37:50 +0530

    Time: 571 ms (73.77%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,2 +1,8 @@
-# Write your MySQL query statement below
-DELETE P1 FROM PERSON P1 JOIN PERSON P2 ON P1.EMAIL=P2.EMAIL AND P1.ID > P2.ID;
+/* Write your PL/SQL query statement below */
+DELETE FROM PERSON
+WHERE ID IN 
+        (SELECT i.ID FROM 
+            (SELECT ID,EMAIL,ROW_NUMBER() OVER (PARTITION BY EMAIL ORDER BY ID) AS rn
+             FROM PERSON) i
+             WHERE i.rn>1
+);
