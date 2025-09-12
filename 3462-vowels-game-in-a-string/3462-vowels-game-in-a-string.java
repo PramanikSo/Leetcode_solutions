a360dde31553ade3a14da4f79fe107aa6abf21e2
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-09-12 23:47:46 +0530

    Time: 3 ms (96.06%), Space: 45.9 MB (16.8%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -12,3 +12,8 @@ class Solution {
 
     }
 }
+// only possibility of BOB win is when there is no vowel
+// if odd number of vowel, alice can pick the whole string
+// if even(n) number of vowel then Alice will pick string with (n-1) vowels.For BOB there
+// will be one vowel left so BOB lost (12 <- even. Alice will pick 11 vowels and win as there
+// is insufficient number of vowel left for BOB for taking even number of vowel)
