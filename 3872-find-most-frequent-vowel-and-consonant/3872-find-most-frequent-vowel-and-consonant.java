7238ef61b4dcd028fc18d9d1aa78fb2b934af1f4
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-09-13 22:00:55 +0530

    Time: 1 ms (100%), Space: 42.4 MB (76.14%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -2,22 +2,18 @@ class Solution {
     public int maxFreqSum(String s) {
         int []conCount=new int[26];
         int []voCount=new int[26];
-
+        int voMax=0,conMax=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch =='a' || ch=='e' || ch =='i' || ch=='o' || ch=='u'){
                 voCount[ch-97]++;
+                voMax=Math.max(voMax,voCount[ch-97]);
             }else{
                  conCount[ch-97]++;
+                 conMax=Math.max(conMax,conCount[ch-97]);
             }
            
         }
-       int voMax=0,conMax=0;
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-         voMax=Math.max(voMax,voCount[ch-97]);
-         conMax=Math.max(conMax,conCount[ch-97]);
-       }
        return voMax+conMax;
     }
 }
