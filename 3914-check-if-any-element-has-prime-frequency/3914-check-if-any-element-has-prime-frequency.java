abcd25b93e86fdfd152bdcb89d01ec5358e0baa5
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-09-24 00:17:47 +0530

    Time: 1 ms (96.45%), Space: 42.6 MB (82.74%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
@@ -1,31 +1,24 @@
 class Solution {
     public boolean checkPrimeFrequency(int[] nums) {
-        int n=100;
-        int []primeList = new int[n+1];
-        sieveAlgo(n,primeList);
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        } 
-        for(Map.Entry<Integer,Integer>elem: map.entrySet()){
-            if(primeList[elem.getValue()]==1){
+        int n=nums.length;
+        int cnt[]=new int[100];
+        for(int i=0;i<n;i++){
+            cnt[nums[i]%100]+=1;
+        }
+        for(int i=0;i<100;i++){
+            if(cnt[i]<2){
+                continue;
+            }
+            int x=0;
+            for(int j=2;j*j<=cnt[i];j++){
+                if(cnt[i]%j==0){
+                    x++;
+                }
+            }
+            if(x==0){
                 return true;
             }
         }
         return false;
-    }
-    private void sieveAlgo(int n, int[] primeList) {
-        // Initialize all as prime
-        Arrays.fill(primeList, 1);
-        primeList[0] = 0;
-        primeList[1] = 0;
-
-        for (int i = 2; i * i <= n; i++) {
-            if (primeList[i] == 1) {
-                for (int j = i * i; j <= n; j += i) {
-                    primeList[j] = 0;
-                }
-            }
-        }
     }
 }
