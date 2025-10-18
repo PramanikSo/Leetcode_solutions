901d234edaa085bdc70cb863dddd7c5013221c81
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-10-18 20:04:21 +0530

    Time: 22 ms (6.18%), Space: 41.2 MB (31.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,38 @@
 class Solution {
     public int mySqrt(int x) {
-        double epsilon=0.00001;
-
-        double root=x;
-
-        while(Math.abs(root*root-x)>epsilon){
-            root=(root+x/root)/2;
+        int ans=x;
+        for(int i=1;i<x;i++){
+            if((long)i*i<=x){
+                ans=i;
+            }else{
+                break;
+            }
         }
-        return (int)root;
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+// public int mySqrt(int x) {
+//         double epsilon = 0.00001;
+//         double root = x;
+
+//         while(Math.abs(root*root-x)>epsilon){
+//             root=(root + x / root) / 2;
+//         }
+//         return (int)root;
+//     }
+
+// Used Newton's-Ralphson Method 
+// Xn+1 = Xn - f(Xn) / f'(Xn)
+
+// For square root of N:  f(X) = X^2 - N
+// Using the above formula we get: Xn+1 = ( Xn + N /Xn ) / 2
+
+// We take the value of epsilon = 0.00001 ( We can change accordingly )
