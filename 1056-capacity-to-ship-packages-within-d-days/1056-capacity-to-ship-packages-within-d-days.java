9cd4b4de86cce4f380ed7347ed7ba9164f35dfaa
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-10-25 18:42:56 +0530

    Time: 10 ms (93.42%), Space: 47.3 MB (49.38%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -9,24 +9,20 @@ class Solution {
         while(start<=end){
             int mid=start+(end-start)/2;
             int total=0;
-            int dayCount=0;
+            int dayCount=1;
             for(int i=0;i<n;i++){
                 if(weights[i]>mid){
                     dayCount=days+1;
                     break;
                 }
-                total+=weights[i];
-                if(total>mid){
+                if(total+weights[i]>mid){
                     dayCount++;
                     total=weights[i];
-                }else if(total==mid){
-                     dayCount++;
-                     total=0;
+                }else{
+                    total+=weights[i];
                 }
             }
-            if(total>0){
-                dayCount++;
-            }
+            
             if(dayCount>days){
                 start=mid+1;
             }else{
