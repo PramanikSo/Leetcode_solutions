41829b42ba62e0cf33efc77e0ce6f66f51f8f049
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-11-04 21:50:07 +0530

    Time: 385 ms (53.39%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT
-score,
-DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
-FROM  Scores;
+    Score,
+    DENSE_RANK() OVER (ORDER BY Score DESC) AS rank
+FROM
+    Scores;
