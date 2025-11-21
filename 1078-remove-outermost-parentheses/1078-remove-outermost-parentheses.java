40d4f02ff44f5d0b0acbcd7c2012a033a4233134
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2025-11-21 08:42:38 +0530

    Time: 4 ms (56.84%), Space: 43.3 MB (35.85%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -5,16 +5,10 @@ class Solution {
 
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='('){
-                if(count>0){
+            if(ch=='(' && count++>0){
                     ans.append(ch);
-                }
-                count++;
-            }else{
-                count--;
-                if(count>0){
+            }else if(ch==')' && count-->1){
                     ans.append(ch);
-                }
             }
         }
         return ans.toString();
