4964dbf0ebdad460a1a0f3ec8f65720ee9b61b99
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-01-04 21:08:36 +0530

    Time: 0 ms (100%), Space: 43.3 MB (29.8%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -20,32 +20,32 @@ class Solution {
         return ans;
     }
     // Iterative
-    private void preOrder(TreeNode node, List<Integer>ans){
-        if(node==null){
-            return;
-        }
-        Stack<TreeNode> stack=new Stack();
-        stack.push(node);
-        while(!stack.isEmpty()){
-            TreeNode currNode = stack.pop();
-            ans.add(currNode.val);
-            if(currNode.right!=null){
-                stack.push(currNode.right);
-            }
-            if(currNode.left!=null){
-                stack.push(currNode.left);
-            }
-
-        }
-    }
-
-    // Recursive
     // private void preOrder(TreeNode node, List<Integer>ans){
     //     if(node==null){
     //         return;
     //     }
-    //     ans.add(node.val);
-    //     preOrder(node.left,ans);
-    //     preOrder(node.right,ans);
+    //     Stack<TreeNode> stack=new Stack();
+    //     stack.push(node);
+    //     while(!stack.isEmpty()){
+    //         TreeNode currNode = stack.pop();
+    //         ans.add(currNode.val);
+    //         if(currNode.right!=null){
+    //             stack.push(currNode.right);
+    //         }
+    //         if(currNode.left!=null){
+    //             stack.push(currNode.left);
+    //         }
+
+    //     }
     // }
+
+    // Recursive
+    private void preOrder(TreeNode node, List<Integer>ans){
+        if(node==null){
+            return;
+        }
+        ans.add(node.val);
+        preOrder(node.left,ans);
+        preOrder(node.right,ans);
+    }
 }
