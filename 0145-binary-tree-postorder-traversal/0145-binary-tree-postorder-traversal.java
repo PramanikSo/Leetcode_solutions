a6c8fe069c954afd219749b0b03a8c94b55f6fbd
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-01-15 21:30:26 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.41%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -16,7 +16,8 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer>ans=new ArrayList<>();
-        postOrder(ans,root);
+        // postOrder(ans,root);
+        iterativePostOrderUsingTwoStack(ans,root);
         return ans;
     }
 
@@ -26,5 +27,27 @@ class Solution {
         postOrder(ans,curr.left);
         postOrder(ans,curr.right);
         ans.add(curr.val);
+    }
+
+    private void iterativePostOrderUsingTwoStack(List<Integer>ans,TreeNode curr){
+        if(curr==null)  return;
+        Stack<TreeNode>s1=new Stack<>();
+        Stack<TreeNode>s2=new Stack<>();
+        s1.push(curr);
+
+        while(!s1.isEmpty()){
+            curr=s1.pop();
+            s2.push(curr);
+            if(curr.left!=null){
+                s1.push(curr.left);
+            }
+            if(curr.right!=null){
+                s1.push(curr.right);
+            }
+        }
+
+        while(!s2.isEmpty()){
+            ans.add(s2.pop().val);
+        }
     }
 }
