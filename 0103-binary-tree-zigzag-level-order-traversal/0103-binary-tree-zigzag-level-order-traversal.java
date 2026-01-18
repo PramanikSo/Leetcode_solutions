1e169dbd34acbd20734b1fcbca092a107adc2acd
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-01-18 20:20:40 +0530

    Time: 1 ms (82.64%), Space: 43.8 MB (29.1%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,29 +16,23 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
-         List<List<Integer>>ans=new LinkedList<>();
+         List<List<Integer>>ans=new ArrayList<>();
         if(root==null) return ans;
-        Queue<TreeNode>q=new LinkedList<>();
+        Deque<TreeNode> q = new ArrayDeque<>();
         q.add(root);
-        boolean flag=true;
+        boolean leftToRight=true;
         while(!q.isEmpty()){
             int size=q.size();
             List<Integer>list=new LinkedList<>();
             for(int i=0;i<size;i++){
                 TreeNode curr=q.poll();
-                if(flag){
-                    list.add(curr.val);
-                }else{
-                    list.addFirst(curr.val);
-                }
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right!=null){
-                    q.add(curr.right);
-                }
+                if(leftToRight)    list.add(curr.val);
+                else    list.addFirst(curr.val);
+
+                if(curr.left!=null)  q.add(curr.left);
+                if(curr.right!=null)    q.add(curr.right);
             }
-            flag=!flag;
+            leftToRight =!leftToRight ;
             ans.add(list);
         }
         return ans;
