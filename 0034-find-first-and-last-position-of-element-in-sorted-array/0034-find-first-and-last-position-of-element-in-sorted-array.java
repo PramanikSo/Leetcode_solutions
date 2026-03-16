17648ef16bce939df1914777739f26b9375dfd4f
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-03-16 09:14:55 +0530

    Time: 0 ms (100%), Space: 48.3 MB (25.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,13 +2,11 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int start=0;
         int end=nums.length-1;
-        int []ans={-1,-1};
+        int []ans=new int[]{-1,-1};
         while(start<=end){
             int mid=start+(end-start)/2;
 
             if(nums[mid]==target){
-                ans[0]=mid;
-                ans[1]=mid;
                 int temp=mid;
                 while(temp>=0 && nums[temp]==target){
                     ans[0]=temp;
@@ -19,8 +17,7 @@ class Solution {
                     mid++;
                 }
                 return ans;
-            }
-            else if(nums[mid]>target){
+            }else if(nums[mid]>target){
                 end=mid-1;
             }else{
                 start=mid+1;
