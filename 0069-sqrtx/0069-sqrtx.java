3140e56fe51acb457999635500758e86c06c7901
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-03-20 10:03:47 +0530

    Time: 1 ms (97.45%), Space: 42.9 MB (18.48%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,45 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-       //using binary search
-       int start=1;
-       int end=x;
-
-       while(start<=end){
-         int mid=start+(end-start)/2;
-         long prod = (long)mid*mid;
-         if(prod==x){
-            return mid;
-         }else if(prod<x){
-            start=mid+1;
-         }else{
-            end=mid-1;
-         }
-       }
-       return end;
+        int start=1;
+        int end=x;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            long product = (long)mid*mid;
+            if(product==x){
+                return mid;
+            }else if(product<x){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return end;
     }
 }
-
-
-
-
-
-
-
-
-// public int mySqrt(int x) {
-//         double epsilon = 0.00001;
-//         double root = x;
-
-//         while(Math.abs(root*root-x)>epsilon){
-//             root=(root + x / root) / 2;
-//         }
-//         return (int)root;
-//     }
-
-// Used Newton's-Ralphson Method 
-// Xn+1 = Xn - f(Xn) / f'(Xn)
-
-// For square root of N:  f(X) = X^2 - N
-// Using the above formula we get: Xn+1 = ( Xn + N /Xn ) / 2
-
-// We take the value of epsilon = 0.00001 ( We can change accordingly )
