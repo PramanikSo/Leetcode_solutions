c2953b897202f3f572c2fd2a3a7015ba39e495c7
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-03-21 09:22:04 +0530

    Time: 18 ms (9.43%), Space: 41.9 MB (91.3%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n <=1)   return n;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fibUsingDp(n,dp);
+    }
 
-        return fib(n-1) + fib(n-2);
+    int fibUsingDp(int n,int[]dp){
+        if(n<=1)    return n;
+        if(dp[n]!=-1)   return dp[n];
+        return fibUsingDp(n-1,dp)+fibUsingDp(n-2,dp);
     }
 }
