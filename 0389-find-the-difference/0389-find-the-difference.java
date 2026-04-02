7f53f2e4fde8aaaec0a0aebc4f42a3be3226b351
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-04-02 08:45:33 +0530

    Time: 2 ms (61.86%), Space: 42.8 MB (95.02%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,18 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         int n=s.length();
-        int count[]=new int[26];
+        char xor=0;
         for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            count[ch-'a']++;
+            xor^=s.charAt(i);
         }
         for(int i=0;i<=n;i++){
-            char ch=t.charAt(i);
-            count[ch-'a']--;
-            if(count[ch-'a']<0){
-                return ch;
-            }
+             xor^=t.charAt(i);
         }
-        return ' ';
+        return xor;
     }
 }
