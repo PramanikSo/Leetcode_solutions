87b25cec9ab482ff89fe7acab4eec6d6134cf436
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-06-21 10:47:43 +0530

    Time: 0 ms (100%), Space: 48 MB (85.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int first=getLowerBound(nums,target);
+        int last=getUpperBound(nums,target);
+        return new int[]{first,last};
+    }
+
+    int getLowerBound(int []nums,int target){
         int start=0;
         int end=nums.length-1;
-        int []ans={-1,-1};
+        int ans=-1;
         while(start<=end){
-            int mid=start+(end-start);
+            int mid=start+(end-start)/2;
 
-            if(nums[mid]>target){
+            if(nums[mid]==target){
+                ans=mid;
                 end=mid-1;
-            }else if(nums[mid]<target){
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            } 
+        }
+        return ans;
+    }
+
+    int getUpperBound(int []nums, int target){
+        int start=0;
+        int end=nums.length-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target){
                 start=mid+1;
             }else{
-                ans[0]=mid;
-                ans[1]=mid;
-                int i=mid;
-                while(i>=0 && nums[i]==target){
-                    ans[0]=i;
-                    i--;
-                }
-                while(mid<nums.length && nums[mid]==target){
-                    ans[1]=mid;
-                    mid++;
-                }
-                return ans;
-            }
+                end=mid-1;
+            } 
         }
-          return ans;
+        return ans;
     }
 }
