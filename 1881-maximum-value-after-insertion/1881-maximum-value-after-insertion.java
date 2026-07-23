3ed173226d61ec115d94f845ac632bdcbe182692
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-07-23 07:45:44 +0530

    Time: 1260 ms (5.61%), Space: 53.9 MB (10.28%) - LeetHub

---
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String maxValue(String n, int x) {
+        char sign=n.charAt(0);
+        if(sign=='-'){
+            return helper1(n,x);
+        }
+        return helper2(n,x);
+
+    }
+    String helper1(String n, int x){
+        String ans="-";
+        for(int i=1;i<n.length();i++){
+            char ch=n.charAt(i);
+            if(Integer.parseInt(ch+"")>x){
+                ans+=x;
+                ans+=n.substring(i);
+                return ans;
+            }else{
+                ans+=ch;
+            }
+        }
+        return ans+=x;
+    }
+
+     String helper2(String n, int x){
+        String ans="";
+        for(int i=0;i<n.length();i++){
+            char ch=n.charAt(i);
+            if(Integer.parseInt(ch+"")<x){
+                ans+=x;
+                ans+=n.substring(i);
+                return ans;
+            }else{
+                ans+=ch;
+            }
+        }
+        return ans+=x;
+    }
+}
