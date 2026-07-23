12d0853d6c9f1081d34b1e09a426f7c9044dcaed
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-07-23 08:08:55 +0530

    Time: 6 ms (94.39%), Space: 47.9 MB (59.81%) - LeetHub

--- 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.java
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.java
@@ -1,39 +1,28 @@
 class Solution {
     public String maxValue(String n, int x) {
         char sign=n.charAt(0);
+         StringBuilder ans= new StringBuilder(n);
         if(sign=='-'){
-            return helper1(n,x);
-        }
-        return helper2(n,x);
-
-    }
-    String helper1(String n, int x){
-        String ans="-";
-        for(int i=1;i<n.length();i++){
-            char ch=n.charAt(i);
-            if(ch-'0'>x){
-                ans+=x;
-                ans+=n.substring(i);
-                return ans;
-            }else{
-                ans+=ch;
+            for(int i=1;i<ans.length();i++){
+                char ch=ans.charAt(i);
+                if(ch-'0'>x){
+                    ans.insert(i,x);
+                    return ans.toString();
+                }
             }
+            ans.append(x);
+            return ans.toString();
         }
-        return ans+=x;
-    }
-
-     String helper2(String n, int x){
-        String ans="";
-        for(int i=0;i<n.length();i++){
-            char ch=n.charAt(i);
-            if(ch-'0'<x){
-                ans+=x;
-                ans+=n.substring(i);
-                return ans;
-            }else{
-                ans+=ch;
+            
+           for(int i=0;i<ans.length();i++){
+                char ch=ans.charAt(i);
+                if(ch-'0'<x){
+                    ans.insert(i,x);
+                    return ans.toString();
+                }
             }
-        }
-        return ans+=x;
+            ans.append(x);
+            return ans.toString();
+
     }
 }
