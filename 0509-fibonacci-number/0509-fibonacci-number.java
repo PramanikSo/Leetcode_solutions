d1cedbb7cc8f5dae4521f03f85a5db4fabba15aa
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-12 09:09:40 +0530

    Time: 42 ms (9.47%), Space: 46.2 MB (16.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,26 @@
 class Solution {
     public int fib(int n) {
-        // base case
-        if(n==0 || n==1)    return n;
+        int []dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fibDP(n,dp);
+    }
 
-        return fib(n-1)+fib(n-2);
+    int fibDP(int n, int []dp){
+        // base case
+        if(n==0 || n==1)    return dp[n]=n;
+
+        int prev;
+        if(dp[n-1]!=-1){
+            prev=dp[n-1];
+        }else{
+            prev=fib(n-1);
+        }
+        int secondPrev;
+        if(dp[n-2]!=-1){
+            secondPrev=dp[n-2];
+        }else{
+            secondPrev=fib(n-2);
+        }
+        return dp[n]=prev+secondPrev;
     }
 }
