e071ecbdd686c8cf09e04021c47961ccf16a0c24
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-12 09:15:04 +0530

    Time: 0 ms (100%), Space: 42 MB (70.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,16 @@
 class Solution {
     public int fib(int n) {
         int []dp=new int[n+1];
+        /* ==== for recursion
         Arrays.fill(dp,-1);
         return fibDP(n,dp);
+        */
+        if(n<=1)    return n;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 
     int fibDP(int n, int []dp){
