16c3f11a7bb48bac978447c971533215b91e7889
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-12 10:04:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-       return climbStairsDP(n,dp);
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //    return climbStairsDP(n,dp);
+
+            return iterativeDP(n);
+    }
+    int iterativeDP(int n){
+         if(n<=2)    return n;
+        int []dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 
     int climbStairsDP(int n,int[]dp){
