9126e70f03a0a69d1343e50052be4e0db3f56cfe
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-13 19:46:24 +0530

    Time: 5 ms (24.94%), Space: 50.4 MB (38.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,11 +3,33 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int [][]dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return helper(m-1,n-1,grid,dp);
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return helper(m-1,n-1,grid,dp);
+        return helperIterative(m,n,grid,dp);
+    }
 
+    int helperIterative(int m, int n,int [][]grid,int[][]dp){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                }else{   
+
+                    int up=Integer.MAX_VALUE;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    int left=Integer.MAX_VALUE;
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=grid[i][j]+Math.min(up,left);
+                }    
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     int helper(int i, int j,int [][]grid,int[][]dp){
