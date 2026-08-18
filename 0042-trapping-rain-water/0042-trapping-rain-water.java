980c02e515e5a992cc6265a73719f578e76369d8
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-18 10:03:26 +0530

    Time: 1 ms (58.59%), Space: 48.7 MB (22.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int [] prefixSum = new int[n];
-        int [] suffixSum = new int[n];
+        int n=height.length;
+        int []suffixSum=new int[n];
+        int []prefixSum=new int[n];
+
         prefixSum[0]=height[0];
         suffixSum[n-1]=height[n-1];
-
         for(int i=1;i<n;i++){
-            prefixSum[i] = Math.max(height[i], prefixSum[i-1]);
-            suffixSum[n-1-i] = Math.max(height[n-1-i],suffixSum[n-i]);
+            prefixSum[i]=Math.max(height[i],prefixSum[i-1]);
+            suffixSum[n-1-i]=Math.max(height[n-1-i],suffixSum[n-i]);
         }
-        int total = 0;
-        for(int i=0;i<n;i++){
-            int leftMax = prefixSum[i];
-            int rightMax = suffixSum[i];
-            if(height[i]<leftMax && height[i]<suffixSum[i]){
-                total += Math.min(leftMax,rightMax) - height[i];
+        int total=0;
+        for(int i=1;i<n;i++){
+            int leftMax=prefixSum[i];
+            int rightMax=suffixSum[i];
+            if(height[i]<leftMax && height[i]<rightMax){
+                total+=Math.min(leftMax,rightMax)-height[i];
             }
         }
         return total;
