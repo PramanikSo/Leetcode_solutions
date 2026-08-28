78f4d25af2b77efeb73a55760198e1f48bfebda4
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-28 18:14:42 +0530

    Time: 44 ms (16.44%), Space: 46.3 MB (65.28%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -2,124 +2,90 @@ class Solution {
     public long subArrayRanges(int[] nums) {
         return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
     }
-    private long sumSubarrayMins(int[] arr) {
+
+    long sumSubarrayMins(int []nums){
         long sum=0;
-        int []nse=findNSE(arr);
-        int[]pse=findPSE(arr);
-        for(int i=0;i<arr.length;i++){
-            int left = i - pse[i];
-            int right = nse[i]-i;
-            long freq = left*right*1L;
-            long val =(arr[i]*freq);
-            sum=(sum+val);
+        int []pse=findPSE(nums);
+        int []nse=findNSE(nums);
+        for(int i=0;i<nums.length;i++){
+            int left=i-pse[i];
+            int right=nse[i]-i;
+            long freq=left*right*1L;
+            long val=freq*nums[i];
+            sum+=val;
         }
         return sum;
     }
-     private long sumSubarrayMaxs(int[] arr) {
+    long sumSubarrayMaxs(int []nums){
         long sum=0;
-        int []nge=findNGE(arr);
-        int[]pge=findPGE(arr);
-        for(int i=0;i<arr.length;i++){
-            int left = i - pge[i];
-            int right = nge[i]-i;
-            long freq = left*right*1L;
-            long val =(arr[i]*freq);
-            sum=(sum+val);
+        int []pge=findPGE(nums);
+        int []nge=findNGE(nums);
+        for(int i=0;i<nums.length;i++){
+            int left=i-pge[i];
+            int right=nge[i]-i;
+            long freq=left*right*1L;
+            long val=freq*nums[i];
+            sum=sum+val;
         }
         return sum;
     }
 
-       // Function to find indices of Next Smaller Element (NSE)
-    private int[] findNSE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
+    int []findPSE(int []nums){
+        Stack<Integer>stack=new Stack<>();
+        int n=nums.length;
+        int []pse=new int[n];
 
-        // Traverse array from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            // Pop elements that are greater or equal to current
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]>nums[i]){
+                stack.pop();
             }
-
-            // If stack is empty, NSE doesn't exist → set to n
-            ans[i] = !st.isEmpty() ? st.peek() : n;
-
-            // Push current index to stack
-            st.push(i);
+            pse[i]=stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
         }
+        return pse;
+    }
+    int []findNSE(int []nums){
+        Stack<Integer>stack=new Stack<>();
+        int n=nums.length;
+        int []nse=new int[n];
 
-        // Return NSE indices
-        return ans;
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]>=nums[i]){
+                stack.pop();
+            }
+            nse[i]=stack.isEmpty()?n:stack.peek();
+            stack.push(i);
+        }
+        return nse;
     }
 
-    // Function to find indices of Previous Smaller or Equal Element (PSEE)
-    private int[] findPSE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
+       int []findPGE(int []nums){
+        Stack<Integer>stack=new Stack<>();
+        int n=nums.length;
+        int []pge=new int[n];
 
-        // Traverse array from left to right
-        for (int i = 0; i < n; i++) {
-            // Pop elements greater than current
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-                st.pop();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
+                stack.pop();
             }
-
-            // If stack is empty, PSEE doesn't exist → set to -1
-            ans[i] = !st.isEmpty() ? st.peek() : -1;
-
-            // Push current index to stack
-            st.push(i);
+            pge[i]=stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
         }
-
-        // Return PSEE indices
-        return ans;
-    }
-    private int[] findNGE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        // Traverse array from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            // Pop elements that are greater or equal to current
-            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
-                st.pop();
-            }
-
-            // If stack is empty, NSE doesn't exist → set to n
-            ans[i] = !st.isEmpty() ? st.peek() : n;
-
-            // Push current index to stack
-            st.push(i);
-        }
-
-        // Return NSE indices
-        return ans;
+        return pge;
     }
 
-    // Function to find indices of Previous Smaller or Equal Element (PSEE)
-    private int[] findPGE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
+        int []findNGE(int []nums){
+        Stack<Integer>stack=new Stack<>();
+        int n=nums.length;
+        int []nge=new int[n];
 
-        // Traverse array from left to right
-        for (int i = 0; i < n; i++) {
-            // Pop elements greater than current
-            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
-                st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]<=nums[i]){
+                stack.pop();
             }
-
-            // If stack is empty, PSEE doesn't exist → set to -1
-            ans[i] = !st.isEmpty() ? st.peek() : -1;
-
-            // Push current index to stack
-            st.push(i);
+            nge[i]=stack.isEmpty()?n:stack.peek();
+            stack.push(i);
         }
-
-        // Return PSEE indices
-        return ans;
+        return nge;
     }
 }
