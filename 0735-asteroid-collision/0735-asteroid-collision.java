a3ab62471149feb3fe4d05a77101b6078356429e
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-28 12:17:37 +0530

    Time: 5 ms (67.44%), Space: 47.1 MB (42.4%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer>stack=new ArrayDeque<>();
+        Stack<Integer>stack=new Stack<>();
+
         for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]>0)  stack.push(asteroids[i]);
-            else{
-                while(!stack.isEmpty() && stack.peek()>0 && Math.abs(asteroids[i]) > stack.peek()){
+            if(asteroids[i]>0){
+                stack.push(asteroids[i]);
+            }else{
+                while(!stack.isEmpty() && stack.peek()>0 && Math.abs(asteroids[i])>stack.peek()){
                     stack.pop();
                 }
-                if(!stack.isEmpty() && Math.abs(asteroids[i]) == stack.peek()){
+                if(!stack.isEmpty() && stack.peek()==Math.abs(asteroids[i])){
                     stack.pop();
-                }else if((stack.isEmpty() || stack.peek()<0) ){
+                }else if(stack.isEmpty() || stack.peek()<0){
                     stack.push(asteroids[i]);
                 }
             }
