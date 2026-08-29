594bac48c23f2b2ba100099eb9f99d4e31465161
PramanikSo <123895902+PramanikSo@users.noreply.github.com>
2026-08-29 10:24:44 +0530

    Time: 63 ms (71.22%), Space: 78 MB (54.59%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,45 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Integer>stack=new Stack<>();
         int n=heights.length;
-        int []pse=findPSE(heights);
-        int []nse=findNSE(heights);
         int maxArea=0;
         for(int i=0;i<n;i++){
-            int left=pse[i];
-            int right=nse[i];
-            int area=heights[i]*(right-left-1);
-            maxArea=Math.max(maxArea,area);
+            while(!stack.isEmpty() && heights[stack.peek()]>heights[i]){
+                int elem=heights[stack.pop()];
+                int pse=stack.isEmpty()?-1:stack.peek();
+                int area=elem*(i-pse-1);
+                maxArea=Math.max(maxArea,area);
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+                int elem=heights[stack.pop()];
+                int pse=stack.isEmpty()?-1:stack.peek();
+                int area=elem*(n-pse-1);
+                maxArea=Math.max(maxArea,area);
         }
         return maxArea;
-    }
-
-    int []findPSE(int []arr){
-        Stack<Integer>stack=new Stack<>();
-        int n=arr.length;
-        int []pse=new int[n];
-        for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
-                stack.pop();
-            }
-            pse[i]=stack.isEmpty()?-1:stack.peek();
-            stack.push(i);
-        }
-        return pse;
-
-    }
-
-    int []findNSE(int []arr){
-        Stack<Integer>stack=new Stack<>();
-        int n=arr.length;
-        int []nse=new int[n];
-        for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
-                stack.pop();
-            }
-
-            nse[i]=stack.isEmpty()?n:stack.peek();
-            stack.push(i);
-        }
-        return nse;
     }
 }
